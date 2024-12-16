6e2fa1ec6a1f90a549316c17cae8fe41f9be4b35
Abhay <103443256+Abhay1006@users.noreply.github.com>
2024-12-16 14:04:09 +0530

    Create 1st.cpp
    solution for first question in assignment

---
+++ 1st.cpp
@@ -0,0 +1,40 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1;k<=i*2-1;k++)
+        {
+            cout<<"*";
+        }
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    for(int i=n-1;i>=1;i--)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1;k<=i*2-1;k++)
+        {
+            cout<<"*";
+        }
+        for(int j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
